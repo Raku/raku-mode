Raku/raku-mode

commit af61f783777fb9ba2e51f834c76a128b67009b09

Never look at more than 4k when detecting Perl 6

perl6-mode.el
@@ -77,14 +77,14 @@
            (while keep-going
              (cond ((eq (point) (point-max))
                     (setq keep-going nil))
-                   ((looking-at "^ *\\(?:#.*\\)?$")
+                   ((re-search-forward "^ *\\(?:#.*\\)?$" 4096 'noerror)
                     nil)
-                   ((looking-at perl6-magic-pattern)
+                   ((re-search-forward perl6-magic-pattern 4096 'noerror)
                     (setq keep-going nil
                           found-perl6 t))
                    (t
                     (setq keep-going nil)))
-             (beginning-of-line 2))
+             (search-forward "\n" 4096 'noerror))
            found-perl6))))
 
 ;;;###autoload

commit 1ef289eb4daddacada6879e5f4f2d23a67e89ef6

Consider 4096 be eof when looking for the next line
Here I'm also fixing a bug introduced in af61f78 where it would fail to
skip over whitespace/comments.

perl6-mode.el
@@ -73,18 +73,19 @@
     (and (and (stringp buffer-file-name)
               (string-match "\\.\\(?:t\\|p[lm]\\)\\'" buffer-file-name))
          (let ((keep-going t)
-               (found-perl6 nil))
+               (found-perl6 nil)
+               (max-pos (min 4096 (point-max))))
            (while keep-going
-             (cond ((eq (point) (point-max))
+             (cond ((>= (point) max-pos)
                     (setq keep-going nil))
-                   ((re-search-forward "^ *\\(?:#.*\\)?$" 4096 'noerror)
+                   ((looking-at "^ *\\(?:#.*\\)?$")
                     nil)
-                   ((re-search-forward perl6-magic-pattern 4096 'noerror)
+                   ((looking-at perl6-magic-pattern)
                     (setq keep-going nil
                           found-perl6 t))
                    (t
                     (setq keep-going nil)))
-             (search-forward "\n" 4096 'noerror))
+             (beginning-of-line 2))
            found-perl6))))
 
 ;;;###autoload