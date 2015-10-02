Raku/raku-mode

commit c93daf235313f4388949449b6ab589c65eb587ca

Avoid highlighting X::Foo and X:: as cross-metaoperators

perl6-font-lock.el
@@ -382,8 +382,8 @@ Takes arguments START and END which delimit the region to propertize."
       ((rx (regex "[_[:alnum:]]") (group (any "-'")) (regex "[_[:alpha:]]"))
        (1 "_"))
       ;; same for "::" around identifiers
-      ((rx (or (and "::" symbol-start)
-               (and symbol-end "::")))
+      ((perl6-rx (or (and (opt identifier) "::" symbol-start)
+                     (and identifier "::")))
        (0 "_"))
       ((rx "#" (any "`|="))
        (0 (ignore (perl6-syntax-propertize-embedded-comment))))

commit 1277ec749e58baecc3cbb97a6ab547025e4763a6

Fix highlighting of words in identifers
This regressed in c93daf2. They should have 'word' syntax, not 'symbol'.

perl6-font-lock.el
@@ -382,9 +382,10 @@ Takes arguments START and END which delimit the region to propertize."
       ((rx (regex "[_[:alnum:]]") (group (any "-'")) (regex "[_[:alpha:]]"))
        (1 "_"))
       ;; same for "::" around identifiers
-      ((perl6-rx (or (and (opt identifier) "::" symbol-start)
-                     (and identifier "::")))
-       (0 "_"))
+      ((perl6-rx (or (and (opt identifier) (group "::") symbol-start)
+                     (and identifier (group "::"))))
+       (1 "_")
+       (2 "_"))
       ((rx "#" (any "`|="))
        (0 (ignore (perl6-syntax-propertize-embedded-comment))))
       ((rx "#" (0+ not-newline))