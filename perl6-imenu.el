81805cfa8e1ddc4eafa1381264b6f41d695cbba9
Tom Browder <tom.browder@gmail.com>
2016-07-29 12:21:20 -0500

    remove vars (to main file) per comments from syohex, clean up some

--- perl6-imenu.el
+++ perl6-imenu.el
@@ -1,32 +1,18 @@
 ;;; perl6-imenu.el --- Imenu support Perl 6 -*- lexical-binding: t; -*-
 
-;;; Commentary:
-
-
 ;; Imenu functions and variables are defined here.
 (defvar perl6-imenu-generic-expression
-  '(
-    ;; the names are in reverse desired order since they are evaluated here last first
-    ("Variables" "^\\s-*\\(?:my\\|our\\)\\s-+\\(\\(?:\\$\\|@\\|%\\)\\(?:[_[:alnum:]]+\\)\\)" 1)
-    ("Subs/Methods" "^\\s-*\\(?:my\\s-+\\|our\\s-+\\)?\\(?:multi\\s-+sub\\|multi\\s-+method\\|sub\\|method\\|multi\\)\\s-+\\(.+)\\)" 1)
-    )
-  "Define interesting points in the Perl 6 buffer for `imenu'.
+      '(
+        ;; the names are in reverse desired order since they are evaluated here last first
+        ("Variables" "^\\s-*\\(?:my\\|our\\)\\s-+\\(\\(?:\\$\\|@\\|%\\)\\(?:[_[:alnum:]]+\\)\\)" 1)
+        ("Subs/Methods" "^\\s-*\\(?:my\\s-+\\|our\\s-+\\)?\\(?:multi\\s-+sub\\|multi\\s-+method\\|sub\\|method\\|multi\\)\\s-+\\(.+)\\)" 1)
+        )
+      "Define interesting points in the Perl 6 buffer for `imenu'.
 
 This is used to set `imenu-generic-expression' when Perl 6 mode is
 entered.  Subsequent changes to `perl6-imenu-generic-expression' will
 not affect existing Perl 6 buffers because imenu-generic-expression is
 a local variable.")
-
-;; Add imenu support for perl6-mode.  Note that imenu-generic-expression
-;; is buffer-local, so we don't need a local-variable for it.
-(setq imenu-generic-expression perl6-imenu-generic-expression
-      imenu-case-fold-search nil)
-
-;; Imenu support
-(add-hook 'perl6-mode-hook 'imenu-add-menubar-index)
-(add-hook 'perl6-mode-hook
-	  (lambda ()
-	    (setq imenu-generic-expression perl6-imenu-generic-expression)))
 
 ;;===========================
 (provide 'perl6-imenu)
