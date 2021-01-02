53127cc01422a540d8f9bc0fd3130520e77f438f
Kang-min Liu <gugod@gugod.org>
2021-01-02 23:09:44 +0900

    An attempt to resolve an error on indenting.
    Fix issue #23
    
    When indenting something like
    
        if %h{"foo"} {
            ....... (press tab here)
        }
    
    This error happens.
    
        (error "Lisp nesting exceeds ‘max-lisp-eval-depth’")
    
    Note here that while the fix does makes the error go away, it is not
    entirely certain whether it breaks the indentation for some other
    cases.
    
    But I think we could live with this until we found what's broken.

--- raku-indent.el
+++ raku-indent.el
@@ -55,10 +55,6 @@
     (`(:elem . basic) raku-indent-offset)
     (`(:elem . arg) 0)
     (`(:list-intro . ,(or `";" `"")) t) ;"" stands for BOB (bug#15467).
-    (`(:before . "{")
-     (when (smie-rule-hanging-p)
-       (smie-backward-sexp ";")
-       (smie-indent-virtual)))
     (`(:before . ,(or "{" "("))
      (if (smie-rule-hanging-p) (smie-rule-parent 0)))))
 
